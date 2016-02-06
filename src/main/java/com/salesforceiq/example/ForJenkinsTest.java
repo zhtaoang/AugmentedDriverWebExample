ea12b8acb65bb91c425dc1d7ad6eddf4ea75b111
SenhorCastor <fernando.a.martin@gmail.com>
2016-02-06 14:32:06 -0800

    test for jenkins

---
+++ src/main/java/com/salesforceiq/example/ForJenkinsTest.java
@@ -0,0 +1,22 @@
+package com.salesforceiq.example;
+
+import org.junit.Test;
+
+public class ForJenkinsTest {
+
+    @Test
+    public void testOne() {
+
+    }
+
+    @Test
+    public void testTwo() {
+
+    }
+
+    @Test
+    public void testThree() {
+
+    }
+
+}
--- src/main/java/com/salesforceiq/example/WebSuites.java
+++ src/main/java/com/salesforceiq/example/WebSuites.java
@@ -2,4 +2,5 @@ package com.salesforceiq.example;
 
 public class WebSuites {
     public static final String SMOKE = "SMOKE";
+    public static final String FOR_JENKINS_TEST = "JENKINS";
 }
